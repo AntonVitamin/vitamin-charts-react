082e42c843961f08248d17bd49ad6f970e0e3346
Anton Orro <anton@multivitamin.studio>
2023-11-07 10:59:16 +0000

    Move data objects to data folder

---
+++ src/utils/charts/default-styles/data/index.js
@@ -0,0 +1,29 @@
+export const darkColourPalette = {
+  generic: {
+    labels: '#FFF',
+    borders: '#4F4F4F',
+    titles: {
+      main: '#828282',
+      secondary: '#FFF',
+    },
+  },
+  bar: {
+    border: '#165BAA',
+    background: '#63ABFD',
+  },
+};
+
+export const lightColourPalette = {
+  generic: {
+    labels: '#000',
+    borders: '#D3D3D3',
+    titles: {
+      main: '#828282',
+      secondary: '#A8A8A8',
+    },
+  },
+  bar: {
+    border: '#165BAA',
+    background: '#63ABFD',
+  },
+};
--- src/utils/charts/default-styles/setters/index.js
+++ src/utils/charts/default-styles/setters/index.js
@@ -1,32 +1,4 @@
-const darkColourPalette = {
-  generic: {
-    labels: '#FFF',
-    borders: '#4F4F4F',
-    titles: {
-      main: '#828282',
-      secondary: '#FFF',
-    },
-  },
-  bar: {
-    border: '#165BAA',
-    background: '#63ABFD',
-  },
-};
-
-const lightColourPalette = {
-  generic: {
-    labels: '#000',
-    borders: '#D3D3D3',
-    titles: {
-      main: '#828282',
-      secondary: '#A8A8A8',
-    },
-  },
-  bar: {
-    border: '#165BAA',
-    background: '#63ABFD',
-  },
-};
+import {lightColourPalette, darkColourPalette} from '../data';
 
 export const setChartBarDefaults = (chartInterface) => {
   // colours
