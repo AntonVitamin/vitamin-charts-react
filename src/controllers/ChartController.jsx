580cd90cebab5752785a2deb3da54e7ab3f272ae
Anton Orro <anton@multivitamin.studio>
2023-11-09 15:09:44 +0000

    Add observer to scale title in relation to chart width

--- src/controllers/ChartController.jsx
+++ src/controllers/ChartController.jsx
@@ -1,3 +1,4 @@
+import {useEffect, useRef} from 'react';
 import {BarChart, LineChart, PieChart} from '../components/Charts';
 import {parseChartConfig} from '../utils/charts/data-parsing';
 
@@ -13,15 +14,42 @@ const renderChart = (chartConfig, isDarkMode) => {
   return CHART_MAP[type];
 };
 
-const ChartController = ({chartConfig, isDarkMode}) => {
+const ChartController = (props) => {
+  const {chartConfig, isDarkMode} = props;
+  const containerRef = useRef(null);
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    const parentElement = containerRef?.current;
+    const childElement = titleRef?.current;
+
+    if (!parentElement) return;
+    if (!childElement) return;
+
+    let resizeObserver = new ResizeObserver(() => {
+      childElement.style.fontSize = `${parentElement.clientWidth / 20}px`;
+    });
+
+    resizeObserver.observe(parentElement);
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+
   return (
     <div
+      id="chart-container"
+      ref={containerRef}
       style={{
         position: 'relative',
         margin: 'auto',
         width: '80vw',
         height: '80vh',
       }}>
+      <h3 id="chart-title" ref={titleRef}>
+        {chartConfig.options?.title}
+      </h3>
       {renderChart(chartConfig, isDarkMode)}
     </div>
   );
